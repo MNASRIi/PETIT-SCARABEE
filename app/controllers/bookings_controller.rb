8a18ef56930d42f97986f2cf489ce9bfdc0fc64e
wissam nasreddine <wissam.nasreddine@hec.edu>
2023-02-28 15:53:45 +0100

    changed validation

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,38 +1,38 @@
 class BookingsController < ApplicationController
 
   def create
-  @booking = Booking.new(bookings_params)
-  @lesson = Lesson.find(params[:lesson_id])
-  @booking.lesson = @lesson
-  @booking.user = current_user
-  @authorize
+    @booking = Booking.new(bookings_params)
+    @lesson = Lesson.find(params[:lesson_id])
+    @booking.lesson = @lesson
+    @booking.user = current_user
+    authorize @booking
   end
 
   def accept
     @booking = Booking.find(params[:id])
     @booking.status = "Accepted"
     @booking.save
-    @authorize
+    authorize @booking
   end
 
   def decline
     @booking = Booking.find(params[:id])
     @booking.status = "Declined"
     @booking.save
-    @authorize
+    authorize @booking
   end
 
   def cancel
     @booking = Booking.find(params[:id])
     @booking.status = "Canceled"
     @booking.save
-    @authorize
+    authorize @booking
   end
 
   def destroy
     @booking = Booking.find(params[:id])
     @booking.destroy
-    @authorize
+    authorize @booking
   end
 
 private
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,6 @@ class PagesController < ApplicationController
   def home
   end
 
-  def mybookings
-    @user = current_user
-    @bookings = @user.bookings
-    @authorize
-  end
-
   def dashboard
   end
 
