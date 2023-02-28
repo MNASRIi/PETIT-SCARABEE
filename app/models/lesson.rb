5eb722c448d178bec6e57d1918d7824d0b922e36
wissam nasreddine <wissam.nasreddine@hec.edu>
2023-02-28 12:52:15 +0100

    DA comments on categories and description

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,10 +3,12 @@ class Lesson < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
-  validates :category, presence: true, inclusion in { in: %w(violin guitar singing piano yoga chess crossfit stretching football basketball handball golf swimming),
+  CATEGORIES = %w(violin guitar singing piano yoga chess crossfit stretching football basketball handball golf swimming)
+
+  validates :category, presence: true, inclusion in { in: CATEGORIES,
   message: "%{value} is not a valid category" }
   validates :title, presence: true, length: { minimum: 5 }
-  validates :description, presence: true, length: { minimum: 10 }
+  validates :description, presence: true, length: { minimum: 100 }
   validates :price, presence: true, default: 10
   validates :duration, presence: true, default: 1
   validates :address, presence: true
