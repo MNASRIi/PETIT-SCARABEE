65126202c78813e02ac4ec39e91d1ad7c74cbbbb
Stefan Milosavljevic <stefanva@hawaii.edu>
2023-02-28 16:19:38 +0100

    Resolving issue in navbar_controller

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -4,12 +4,11 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
   }
-}
-
-updateNavbar() {
-  if (window.scrollY >= window.innerHeight) {
-    this.element.classList.add("navbar-lewagon-white")
-  } else {
-    this.element.classList.remove("navbar-lewagon-white")
+  updateNavbar() {
+    if (window.scrollY >= window.innerHeight) {
+      this.element.classList.add("navbar-lewagon-white")
+    } else {
+      this.element.classList.remove("navbar-lewagon-white")
+    }
   }
 }
