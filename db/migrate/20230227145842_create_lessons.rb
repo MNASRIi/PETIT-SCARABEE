b803d95b1a24366d758bdaeef1ce00de45bf814b
delphine abouab <delphine.abouab@outlook.fr>
2023-02-28 11:07:58 +0100

    Pundit init & change address column from User to Lesson

--- db/migrate/20230227145842_create_lessons.rb
+++ db/migrate/20230227145842_create_lessons.rb
@@ -4,6 +4,7 @@ class CreateLessons < ActiveRecord::Migration[7.0]
       t.string :category
       t.string :title
       t.string :description
+      t.string :address
       t.integer :price
       t.integer :duration
       t.references :user, null: false, foreign_key: true
