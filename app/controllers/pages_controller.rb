af8a8837ae95166e374b837f185f5fcd96e64a5a
delphine abouab <delphine.abouab@outlook.fr>
2023-03-02 14:01:53 +0100

    init popup profile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,7 @@ class PagesController < ApplicationController
     @user = current_user
     mybookings
     mylessons
+    popup
   end
 
   def mybookings
@@ -18,6 +19,11 @@ class PagesController < ApplicationController
   def mylessons
     @mylessons = current_user.lessons
     @bookings = current_user.requested_bookings
+  end
+
+  def popup
+      @users = User.all
+      @card = @users.map { |user| { profile_card_html: render_to_string(partial: "shared/profile_popup", locals: {user: flat}) } }
   end
 
 end
